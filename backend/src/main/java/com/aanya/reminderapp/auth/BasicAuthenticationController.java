932b29c3908345108e95087f18ee2e3d5a086070
Aanya <aanyasingh604@gmail.com>
2021-06-29 13:18:58 -0500

    added authentication

---
+++ backend/src/main/java/com/aanya/reminderapp/auth/BasicAuthenticationController.java
@@ -0,0 +1,16 @@
+package com.aanya.reminderapp.auth;
+
+//import com.aanya.reminderapp.auth.AuthenticationBean;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@CrossOrigin(origins = "http://localhost:3000")
+//@RestController
+//public class formerBasicAuthenticationController {
+//
+//    @GetMapping(path = "/basicauth")
+//    public AuthenticationBean helloWorldBean() {
+//        return new AuthenticationBean("You are authenticated");
+//    }
+//}
